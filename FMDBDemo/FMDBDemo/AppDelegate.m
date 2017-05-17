8ed02da08db8eeb727972b1f268816ea2b706d71
coderonepolo <278166530@qq.com>
2017-05-17 17:15:57 +0800

    commit
    commit

--- FMDBDemo/FMDBDemo/AppDelegate.m
+++ FMDBDemo/FMDBDemo/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    NSLog(@"application");
     
      self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
